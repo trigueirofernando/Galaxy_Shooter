6d75c7a41051ecfe25d18c0f03dc814210935b7d
Fernando Trigueiro <trigueirofernando@hotmail.com>
2020-08-11 22:38:32 -0300

    Configurado movimentacao e limite de movimento do player

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -8,9 +8,10 @@ public class Player : MonoBehaviour
     //Data type ( int, float, bool, string)
     //Espaço para as variáveis
 
-    public float speed = 5.0f;
-    
-    
+    //SerializeField serve para mesmo a variável estando private
+    //podermos acessar no Inspector do Unity para alterar seu valor
+    [SerializeField]
+    private float speed = 5.0f;
     
     // Start is called before the first frame update
     // O Start será chamado no começo do jogo apenas uma vez
@@ -24,12 +25,40 @@ public class Player : MonoBehaviour
     // É chamado uma vez a cada frame, após o Start
     void Update()
     {
+        Movement();
+    }
+
+    private void Movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
         transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
 
-        Debug.Log("Horizontal: " + horizontalInput);
-        Debug.Log("vertical: " + verticalInput);
+        //Se o 'player' no eixo Y for maior que 0
+        //Setto a posição do 'player' no eixo Y para 0
+        //Restringindo que não saia da tela ao movimentar
+
+        //Posição y = cima e baixo, não passar do limite inferior de -4.2 e superior de 0
+        //Limite superior de 0 para ficar no meio da tela.
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        //Posição x = esquerda e direita, não passar do limite da tela de 8.5
+        if (transform.position.x > 8.5f)
+        {
+            transform.position = new Vector3(8.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8.5f)
+        {
+            transform.position = new Vector3(-8.5f, transform.position.y, 0);
+        }
     }
+
 }
